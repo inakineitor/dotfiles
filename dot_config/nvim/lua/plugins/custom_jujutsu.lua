66ea5cf0f3779e7cffd00fafd0f7c379d9a35169
Iñaki Arango <arangoinaki@gmail.com>
2026-04-22 16:30:22 -0700

    changed jujutsu shortcut

--- dot_config/nvim/lua/plugins/custom_jujutsu.lua
+++ dot_config/nvim/lua/plugins/custom_jujutsu.lua
@@ -4,5 +4,15 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
+    keys = {
+      {
+        -- Default is <Leader>jj
+        -- An example of a custom mapping to open the interface
+        "<Leader>gj",
+        function()
+          require("lazyjui").open()
+        end,
+      },
+    },
   },
 }
